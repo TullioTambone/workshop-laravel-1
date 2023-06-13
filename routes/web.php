25f8ec293f33ab8fee6eb0087b357c3900f1c28b
Your Namede <edoardo.monti.bo@gmail.com>
2023-06-13 16:11:09 +0200

    creazione controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PizzaController as PizzaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/', [PizzaController::class, 'getPizza']);
