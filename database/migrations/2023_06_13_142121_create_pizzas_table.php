d2d02939dadeed52416597b61688b81c3bffc40c
alecasentini <ale.casentini@gmail.com>
2023-06-13 16:31:32 +0200

    creazione tabella

---
+++ database/migrations/2023_06_13_142121_create_pizzas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pizzas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->string('type', 30);
+            $table->integer('hydration');
+            $table->integer('price');
+            $table->text('ingredients');
+            $table->string('flour_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pizzas');
+    }
+};
